aa53f11bf29a476acbe54998e512b4915f021f50
ocean <849638313@qq.com>
2016-06-18 00:25:00 +0800

    配置cocoa pods

---
+++ swift_two/Pods/Target Support Files/Pods-swift_twoTests/Pods-swift_twoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_swift_twoTests : NSObject
+@end
+@implementation PodsDummy_Pods_swift_twoTests
+@end
---
+++ swift_two/Pods/Target Support Files/Pods-swift_twoUITests/Pods-swift_twoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_swift_twoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_swift_twoUITests
+@end
